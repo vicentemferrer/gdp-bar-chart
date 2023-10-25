017566ad1d02ba1e34d23351859b33381a40fef0
Vicente <vicenteferrer_01@hotmail.com>
2023-10-25 17:57:23 -0300

    Axis added

--- main.js
+++ main.js
@@ -4,16 +4,196 @@ import './style.css'
 
 const w = 900
 const h = 464
-const p = 15
+const p = 45
 
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 
-const svg = d3.select('svg').attr('width', w).attr('height', h)
-
 const { data, from_date, to_date } = filterData(await getData(url))
 
-const xScale = d3.scaleLinear().domain([new Date(from_date), new Date(to_date)]).range([p, w - p])
+const svg = d3.select('svg').attr('width', w).attr('height', h)
+
+const xScale = d3.scaleUtc().domain([new Date(from_date), new Date(to_date)]).range([p, w - p])
 const hScale = d3.scaleLinear().domain([d3.min(data, d => d[1]), d3.max(data, d => d[1])]).range([h, p])
 
+const xAxis = d3.axisBottom(xScale)
+const hAxis = d3.axisLeft(hScale)
+
 svg.selectAll('rect').data(data).enter().append('rect').attr('x', d => xScale(new Date(d[0])))
-  .attr('y', d => hScale(d[1])).attr('width', 2).attr('height', d => h - hScale(d[1]))
+  .attr('y', d => hScale(d[1]) - p).attr('width', 2).attr('height', d => h - hScale(d[1]))
+
+svg.append('g')
+  .attr('transform', `translate(0, ${h - p})`)
+  .call(xAxis)
+
+svg.append('g')
+  .attr('transform', `translate(${p}, 0)`)
+  .call(hAxis)
+
+/*
+
+// eslint-disable-next-line no-unused-vars
+const projectName = 'bar-chart';
+
+// coded by @Christian-Paul
+
+var width = 800,
+  height = 400,
+  barWidth = width / 275;
+
+var tooltip = d3
+  .select('.visHolder')
+  .append('div')
+  .attr('id', 'tooltip')
+  .style('opacity', 0);
+
+var overlay = d3
+  .select('.visHolder')
+  .append('div')
+  .attr('class', 'overlay')
+  .style('opacity', 0);
+
+var svgContainer = d3
+  .select('.visHolder')
+  .append('svg')
+  .attr('width', width + 100)
+  .attr('height', height + 60);
+
+d3.json(
+  'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+)
+  .then(data => {
+    svgContainer
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('x', -200)
+      .attr('y', 80)
+      .text('Gross Domestic Product');
+
+    svgContainer
+      .append('text')
+      .attr('x', width / 2 + 120)
+      .attr('y', height + 50)
+      .text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf')
+      .attr('class', 'info');
+
+    var years = data.data.map(function (item) {
+      var quarter;
+      var temp = item[0].substring(5, 7);
+
+      if (temp === '01') {
+        quarter = 'Q1';
+      } else if (temp === '04') {
+        quarter = 'Q2';
+      } else if (temp === '07') {
+        quarter = 'Q3';
+      } else if (temp === '10') {
+        quarter = 'Q4';
+      }
+
+      return item[0].substring(0, 4) + ' ' + quarter;
+    });
+
+    var yearsDate = data.data.map(function (item) {
+      return new Date(item[0]);
+    });
+
+    var xMax = new Date(d3.max(yearsDate));
+    xMax.setMonth(xMax.getMonth() + 3);
+    var xScale = d3
+      .scaleTime()
+      .domain([d3.min(yearsDate), xMax])
+      .range([0, width]);
+
+    var xAxis = d3.axisBottom().scale(xScale);
+
+    svgContainer
+      .append('g')
+      .call(xAxis)
+      .attr('id', 'x-axis')
+      .attr('transform', 'translate(60, 400)');
+
+    var GDP = data.data.map(function (item) {
+      return item[1];
+    });
+
+    var scaledGDP = [];
+
+    var gdpMax = d3.max(GDP);
+
+    var linearScale = d3.scaleLinear().domain([0, gdpMax]).range([0, height]);
+
+    scaledGDP = GDP.map(function (item) {
+      return linearScale(item);
+    });
+
+    var yAxisScale = d3.scaleLinear().domain([0, gdpMax]).range([height, 0]);
+
+    var yAxis = d3.axisLeft(yAxisScale);
+
+    svgContainer
+      .append('g')
+      .call(yAxis)
+      .attr('id', 'y-axis')
+      .attr('transform', 'translate(60, 0)');
+
+    d3.select('svg')
+      .selectAll('rect')
+      .data(scaledGDP)
+      .enter()
+      .append('rect')
+      .attr('data-date', function (d, i) {
+        return data.data[i][0];
+      })
+      .attr('data-gdp', function (d, i) {
+        return data.data[i][1];
+      })
+      .attr('class', 'bar')
+      .attr('x', function (d, i) {
+        return xScale(yearsDate[i]);
+      })
+      .attr('y', function (d) {
+        return height - d;
+      })
+      .attr('width', barWidth)
+      .attr('height', function (d) {
+        return d;
+      })
+      .attr('index', (d, i) => i)
+      .style('fill', '#33adff')
+      .attr('transform', 'translate(60, 0)')
+      .on('mouseover', function (event, d) {
+        // d or datum is the height of the
+        // current rect
+        var i = this.getAttribute('index');
+
+        overlay
+          .transition()
+          .duration(0)
+          .style('height', d + 'px')
+          .style('width', barWidth + 'px')
+          .style('opacity', 0.9)
+          .style('left', i * barWidth + 0 + 'px')
+          .style('top', height - d + 'px')
+          .style('transform', 'translateX(60px)');
+        tooltip.transition().duration(200).style('opacity', 0.9);
+        tooltip
+          .html(
+            years[i] +
+              '<br>' +
+              '$' +
+              GDP[i].toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') +
+              ' Billion'
+          )
+          .attr('data-date', data.data[i][0])
+          .style('left', i * barWidth + 30 + 'px')
+          .style('top', height - 100 + 'px')
+          .style('transform', 'translateX(60px)');
+      })
+      .on('mouseout', function () {
+        tooltip.transition().duration(200).style('opacity', 0);
+        overlay.transition().duration(200).style('opacity', 0);
+      });
+  })
+  .catch(e => console.log(e));
+
+*/
